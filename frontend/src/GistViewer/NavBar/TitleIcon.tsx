7bbd23b296c1b370ba0beb07add4d836042de295
pkemkes <pkemkes@gmail.com>
2025-01-05 16:51:20 +0100

    Make TitleIcon a link to the main page

--- frontend/src/GistViewer/NavBar/TitleIcon.tsx
+++ frontend/src/GistViewer/NavBar/TitleIcon.tsx
@@ -5,12 +5,15 @@ import Logo from "../../assets/Logo.svg"
 export const TitleIcon = () => {
   const isLightMode = useTheme().palette.mode == "light";
 
-  return <Box sx={{ 
-    flexGrow: 1, 
-    display: "flex", 
-    justifyContent: "center", 
-    flexDirection: "column",
-    mx: "1rem",
+  return <Box
+    component="a"
+    href="/"
+    sx={{ 
+      flexGrow: 1, 
+      display: "flex", 
+      justifyContent: "center", 
+      flexDirection: "column",
+      mx: "1rem",
   }}>
     <img src={Logo} width={90} style={ isLightMode ? {
       filter: "brightness(0) saturate(100%)"
