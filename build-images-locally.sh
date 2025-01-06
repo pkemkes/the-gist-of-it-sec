b58cd127bcfc1ed4a1c10ddb910d0e98c3a5dc88
pkemkes <pkemkes@gmail.com>
2025-01-06 18:55:44 +0100

    Add build of cleanup_bot to bash build script

--- build-images-locally.sh
+++ build-images-locally.sh
@@ -1,6 +1,7 @@
 docker build -t pkemkes/the-gist-of-it-sec-gists-bot -f ./backend/Dockerfile.gists_bot ./backend
 docker build -t pkemkes/the-gist-of-it-sec-telegram-bot -f ./backend/Dockerfile.telegram_bot ./backend
 docker build -t pkemkes/the-gist-of-it-sec-rest-server -f ./backend/Dockerfile.rest_server ./backend
+docker build -t pkemkes/the-gist-of-it-sec-cleanup-bot -f ./backend/Dockerfile.cleanup_bot ./backend
 docker build -t pkemkes/the-gist-of-it-sec-database ./database
 docker build -t pkemkes/the-gist-of-it-sec-chromadb ./chromadb
 docker build -t pkemkes/the-gist-of-it-sec-frontend ./frontend
