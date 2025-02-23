3493f3d179ff56667970a6b6708d11df5a33eaf5
pkemkes <pkemkes@gmail.com>
2025-02-23 17:46:05 +0100

    wip basic solution structure

---
+++ GistBackend/GistBackend.UnitTest/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace GistBackend.UnitTest;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ GistBackend/GistBackend/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GistBackend;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddHostedService<HelloWorldService>();
+            })
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .Build();
+        
+        await host.RunAsync();
+    }
+}
