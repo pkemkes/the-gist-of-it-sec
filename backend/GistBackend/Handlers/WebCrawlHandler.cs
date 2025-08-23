f2403ddd933d75fa627d3b8bb93ef704b9729404
pkemkes <pkemkes@gmail.com>
2025-08-23 11:18:48 +0200

    Remove Accept-Encoding header to see if this fixes timeouts

--- backend/GistBackend/Handlers/WebCrawlHandler.cs
+++ backend/GistBackend/Handlers/WebCrawlHandler.cs
@@ -57,7 +57,7 @@ public class WebCrawlHandler(ILogger<WebCrawlHandler>? logger = null) : IWebCraw
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         ["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
         ["Accept-Language"] = "en-US,en;q=0.9",
-        ["Accept-Encoding"] = "gzip, deflate, br",
+        // ["Accept-Encoding"] = "gzip, deflate, br",  // Let Playwright handle encoding
         ["DNT"] = "1",
         ["Connection"] = "keep-alive",
         ["Upgrade-Insecure-Requests"] = "1",
